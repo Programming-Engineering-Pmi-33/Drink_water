68efab3abcdcb57a312668b269b22924b4228c0a
MykhailoSiomachok <62886029+MykhailoSiomachok@users.noreply.github.com>
2020-10-07 19:07:12 +0300

    Context and models

---
+++ Model/ActivityTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DrinkWater
+{
+    public partial class ActivityTime
+    {
+        public long UserId { get; set; }
+        public TimeSpan WakeUp { get; set; }
+        public TimeSpan GoingToBed { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
