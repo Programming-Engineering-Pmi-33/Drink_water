7c396930b005b6f0766a02c4680fb549fa664f83
OlhaDanyliuk <48630214+OlhaDanyliuk@users.noreply.github.com>
2020-12-16 13:07:28 +0200

    Changed formatter for chart in Main window

--- DrinkWater/NUnitTest_MainWindow/UnitTest1.cs
+++ DrinkWater/NUnitTest_MainWindow/UnitTest1.cs
@@ -1,78 +1,94 @@
-using DrinkWater;
-using DrinkWater.LogReg;
-using DrinkWater.MainServices;
-using DrinkWater.ProfileStatisticsServices;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
-using Npgsql;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace NUnitTest_MainWindow
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DrinkWater;
+    using DrinkWater.LogReg;
+    using DrinkWater.MainServices;
+    using DrinkWater.ProfileStatisticsServices;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Storage;
+    using Npgsql;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Unit tests for checking the functionality of the main service.
+    /// </summary>
     public class Tests
     {
+        /// <summary>
+        /// Create MainService object.
+        /// </summary>
         private MainService main = new MainService();
 
+        /// <summary>
+        /// Setup method.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
-            main = new MainService();
+            this.main = new MainService();
         }
 
+        /// <summary>
+        /// Test for checking method that returns information about session user.
+        /// </summary>
         [Test]
         public void GetUser_Pass()
         {
-            main.SetSessionUser(new SessionUser(1, "Mamonchik"));
-            Assert.AreEqual(main.GetUser().UserId, 1);
+            this.main.SetSessionUser(new SessionUser(1, "Mamonchik"));
+            Assert.AreEqual(this.main.GetUser().UserId, 1);
         }
 
-        
+        /// <summary>
+        /// Check the correct method for adding liquid for valid data.
+        /// </summary>
         [Test]
         public void Add_Pass()
         {
-            using (var transaction =main.db.Database.BeginTransaction())
+            using (var transaction = this.main.db.Database.BeginTransaction())
             {
-                main.SetSessionUser(new SessionUser(1, "Mamonchik"));
+                this.main.SetSessionUser(new SessionUser(1, "Mamonchik"));
                 double amount_before = 0;
-                if (main.GetStatistic().GetDailyStatistics().Find(x => x.FluidIdRef == 1) != null)
+                if (this.main.GetStatistic().GetDailyStatistics().Find(x => x.FluidIdRef == 1) != null)
                 {
-                    amount_before = (double)main.GetStatistic().GetDailyStatistics().Find(x => x.FluidIdRef == 1).Sum;
+                    amount_before = (double)this.main.GetStatistic().GetDailyStatistics().Find(x => x.FluidIdRef == 1).Sum;
                     ValidationLiquid validation = new ValidationLiquid("Water", "100");
                     transaction.CreateSavepoint("SavePoint");
-                    main.Add(validation.GetName(), validation.GetAmount());
-                    double amount_after = (from water in main.db.Statistics
+                    this.main.Add(validation.GetName(), validation.GetAmount());
+                    double amount_after = (from water in this.main.db.Statistics
                                            where water.UserIdRef == 1 && water.Date == DateTime.Now.Date && water.FluidIdRef == 1
                                            select water.FluidAmount).FirstOrDefault();
                     transaction.RollbackToSavepoint("SavePoint");
-                    transaction.Dispose();
-                    Assert.AreNotEqual(amount_after, amount_before); 
-                    
+                    Assert.AreNotEqual(amount_after, amount_before);
                 }
             }
         }
 
+        /// <summary>
+        /// Check the correct method for adding the liquid that didn`t exist in the database.
+        /// </summary>
         [Test]
         public void Add_Fail()
         {
-            main.SetSessionUser(new SessionUser(1, "Mamonchik"));
-            double amount_before = (double)main.GetStatistic().GetDailyStatistics().Count;
+            this.main.SetSessionUser(new SessionUser(1, "Mamonchik"));
+            double amount_before = (double)this.main.GetStatistic().GetDailyStatistics().Count;
             ValidationLiquid validation = new ValidationLiquid("Vine", "100");
-            main.Add(validation.GetName(), validation.GetAmount());
-            double amount_after = (double)main.GetStatistic().GetDailyStatistics().Count;
+            this.main.Add(validation.GetName(), validation.GetAmount());
+            double amount_after = (double)this.main.GetStatistic().GetDailyStatistics().Count;
             Assert.AreEqual(amount_after, amount_before);
         }
 
-
+        /// <summary>
+        /// Test for checking method GetStatistic that return statistics of the current user.
+        /// </summary>
         [Test]
         public void GetStatistic_Pass()
         {
-            main.SetSessionUser(new SessionUser(1, "Mamonchik"));
-            StatisticInfo statisticInfo = main.GetStatistic();
+            this.main.SetSessionUser(new SessionUser(1, "Mamonchik"));
+            StatisticInfo statisticInfo = this.main.GetStatistic();
             Assert.IsTrue(statisticInfo.GetDailyStatistics().Count != 0);
         }
-
     }
 }
